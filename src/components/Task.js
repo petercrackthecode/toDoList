fd10ffe826d4d4a009c00ad96bce1956a5c4b18f
petercrackthecode <nguyenvandung02051999@gmail.com>
2019-09-11 16:04:53 -0700

    Added FontAwesomeIcon.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 import ListGroup from 'react-bootstrap/ListGroup';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
 
 export function Task(props) {
     const shiftTask= async () => {
@@ -12,7 +17,9 @@ export function Task(props) {
 
     return (
         <ListGroup.Item as='li' className={`task-${props.task} mt-1 mb-1`} bsPrefix='media-body'>
-            <input type='checkbox' checked={(props.type === 'completed') ? true : false} className='mr-2' onClick={shiftTask}/>{props.task}
+            <input type='checkbox' checked={(props.type === 'completed') ? true : false} className='mr-2' onClick={shiftTask}/>
+            {props.type === 'completed' ? <strike>{props.task}</strike> : props.task}
+            <FontAwesomeIcon className='float-right mr-3 mt-1' icon={faTrash}/>
         </ListGroup.Item>
     );
 }
